7fe35a844409f3902d8f73094b2e0bf5625bca78
Antoine Gersant <antoine.gersant@lesforges.org>
2016-01-30 12:09:38 -0800

    Update API

--- doc/api.lua
+++ doc/api.lua
@@ -11,13 +11,13 @@ function DateVampire( scene )
 	
 	scene:dialogChoice( { "a", "b" } ); -- returns a or b
 	
-	scene:wait( seconds );
 	
 	scene:playSound();
 	scene:playMusic();
 	
 	-- DONE
-	self:startThread( someFunc );
+	scene:wait( seconds );
+	scene:startThread( someFunc );
 	scene:setDialogSpeed( charactersPerSecond ); -- pass nil for default
 	scene:showDialog( "Hello, welcome to this date", { wobbly = true } );
 	
