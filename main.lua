3dac75a3de5cec3ad98ed154805c6c3054591360
EthanHuntMKD <aleksandarkrstik@gmail.com>
2016-01-30 13:34:12 -0800

    FadeIn/Out Sound/Music

--- main.lua
+++ main.lua
@@ -10,11 +10,43 @@ end
 
 
 gCurrentScene = nil;
+gAssets = {
+	BG = {},
+	CHAR = {},
+	SOUND = {},
+	MUSIC = {},
+}
 
-love.update = function()
+
+
+
+love.load = function()
+	gAssets.BG.monster = love.graphics.newImage("assets/bgs/monster.jpg");
+	gAssets.SOUND.mySound = love.audio.newSource("assets/sounds/mySound.ogg");
+	gAssets.MUSIC.mySound = love.audio.newSource("assets/music/Jump.wav");
+	
+	A = 255;
+end
+
+
+
+love.update = function( dt )
 	if not gCurrentScene then
 		gCurrentScene = Scene.new();
 	end
+	
+	
+--	gCurrentScene.fadeOut( 5 );
+	
+	A = A - ( 255 / 5 * dt );
+	love.graphics.setColor(255, 255, 255, A)
+	
+	
+	gCurrentScene:setBackground( gAssets.BG.monster );
+--	gCurrentScene:playSound( gAssets.SOUND.mySound );
+--	gCurrentScene:playMusic( gAssets.MUSIC.mySound );
+	
+	
 end
 
 love.draw = function()
@@ -23,3 +55,4 @@ love.draw = function()
 	end	
 	gCurrentScene:draw();
 end
+
--- src/Scene.lua
+++ src/Scene.lua
@@ -8,6 +8,41 @@ end
 
 Scene.draw = function( self )
 	love.graphics.print( "Oink oink!", 40, 20 );
+	love.graphics.draw(self.currentBackground, 0, 0);
 end
+
+Scene.setBackground = function( self, imageName )
+	self.currentBackground = imageName;
+end
+
+Scene.playSound = function( self, soundName )
+	self.currentSound = soundName;
+end
+
+Scene.playMusic = function( self, musicName )
+	if ( self.currentMusic and self.currentMusic ~= musicName ) then
+		love.audio.stop( self.currentMusic );
+	end
+	
+	self.currentMusic = musicName;
+--	musicName:setLooping( true );
+
+	love.audio.play(self.currentMusic);
+end
+
+Scene.fadeIn = function( duration )
+	A = 0;
+end
+
+
+Scene.fadeOut = function( duration )
+	A = 255;
+end
+
+
+
+
+
+
 
 return Scene;
