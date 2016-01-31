e9989213caedbb840c878fbbbe456c655df82da1
Antoine Gersant <antoine.gersant@lesforges.org>
2016-01-31 15:29:06 -0800

    Fixed window title

--- conf.lua
+++ conf.lua
@@ -1,5 +1,6 @@
 love.conf = function( options )
 	options.console = true;
+	options.window.title = "Love, Lust, Lucifer";
 	options.window.width = 1280;
 	options.window.height = 720;
 	options.window.resizable = true;
