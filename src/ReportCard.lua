c1a8f7a6aece3f3932a2e07d5aabf6281d64abaf
EthanHuntMKD <aleksandarkrstik@gmail.com>
2016-01-31 15:03:45 -0800

    ReportCard done.

--- src/ReportCard.lua
+++ src/ReportCard.lua
@@ -8,32 +8,26 @@ ReportCard.new = function( scene )
 end
 
 ReportCard.draw = function( self )
-	if not ( self.success and self.score ) then
+	if not ( self.score ) then
 		return;
 	end
 
-	
-	love.graphics.draw( gAssets.BG.reportCard, 0, 0 );
-	
-	
-	
-		love.graphics.setColor( 255, 255, 255, 255 );
-		love.graphics.setFont( gAssets.FONT.introTextFont );
-		
-		
-		love.graphics.printf( self.score * 100 .. "% IN LOVE", 0, 100, 1920, "center", 0, 1, 1, ox, oy, 0, 0 );
-		
-		
-		if ( self.success ) then
-			love.graphics.printf( "SUCCESS!", 0, 700, 1920, "center", 0, 1, 1, ox, oy, 0, 0 );
-		else
-			love.graphics.printf( "FAILURE!", 0, 700, 1920, "center", 0, 1, 1, ox, oy, 0, 0 );
-		end
-		
-		
-    
-		
+	love.graphics.setColor( 255, 0, 0, 255 );
+	love.graphics.rectangle( "fill", 800, 930, -700, -510 * self.score );
 
+	love.graphics.setColor( 255, 255, 255, 255 );
+	love.graphics.draw( gAssets.BG.reportCard, 0, 0 );
+
+	love.graphics.setColor( 255, 255, 255, 255 );
+	love.graphics.setFont( gAssets.FONT.introTextFont );
+
+	love.graphics.printf( self.score * 100 .. "% IN LOVE", 850, 500, 1920, "left", 0, 1, 1, ox, oy, 0, 0 );
+
+	if ( self.success ) then
+		love.graphics.printf( "SUCCESS!", 850, 650, 1920, "left", 0, 1, 1, ox, oy, 0, 0 );
+	else
+		love.graphics.printf( "FAILURE!", 850, 650, 1920, "left", 0, 1, 1, ox, oy, 0, 0 );
+	end
 end
 
 ReportCard.showReport = function( self, success, score )
