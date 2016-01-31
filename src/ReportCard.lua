7d4075a19dc02baf11a18ab5a807f3651f0caa65
EthanHuntMKD <aleksandarkrstik@gmail.com>
2016-01-31 14:39:10 -0800

    Report Card progress.

--- src/ReportCard.lua
+++ src/ReportCard.lua
@@ -12,13 +12,28 @@ ReportCard.draw = function( self )
 		return;
 	end
 
-	if self.alpha then
-		love.graphics.setColor( 255, 255, 255, self.alpha );
+	
+	love.graphics.draw( gAssets.BG.reportCard, 0, 0 );
+	
+	
+	
+		love.graphics.setColor( 255, 255, 255, 255 );
 		love.graphics.setFont( gAssets.FONT.introTextFont );
-		local result = love.graphics.printf( self.introName, 0, 700, 1920, "center", 0, 1, 1, ox, oy, 0, 0 );
+		
+		
+		love.graphics.printf( self.score * 100 .. "% IN LOVE", 0, 100, 1920, "center", 0, 1, 1, ox, oy, 0, 0 );
+		
+		
+		if ( self.success ) then
+			love.graphics.printf( "SUCCESS!", 0, 700, 1920, "center", 0, 1, 1, ox, oy, 0, 0 );
+		else
+			love.graphics.printf( "FAILURE!", 0, 700, 1920, "center", 0, 1, 1, ox, oy, 0, 0 );
+		end
+		
+		
     
-		return result
-	end
+		
+
 end
 
 ReportCard.showReport = function( self, success, score )
