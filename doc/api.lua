ba8b83596be070d58f49cf99ff42ae1df456e9a5
Loveschach <codymalach@hotmail.com>
2016-01-30 17:19:32 -0800

    Update the API

--- doc/api.lua
+++ doc/api.lua
@@ -1,23 +1,24 @@
 function DateVampire( scene )
 
 	-- TODO
+	scene:setCharacter( "characterName" );
+	scene:setCharacterPose( "characterName" );
+	scene:waitForCharacterAnimation();
+
+	scene:stopMusic();
+	
+	-- DONE
 	scene:setBackground( gAssets.BG.mockup );
 	scene:fadeOut( seconds );
 	scene:fadeIn( seconds );
 
-	scene:setCharacter( "characterName" );
-	scene:setCharacterPose( "characterName" );
-	scene:waitForCharacterAnimation();
-	
-	scene:playSound( gAssets.SOUND.mySound );
-	scene:playMusic( gAssets.MUSIC.mySound );
-	
-	-- DONE
 	scene:wait( seconds );
 	scene:startThread( someFunc ); -- returns a thread
 	scene:stopThread( thread );
 	scene:setDialogSpeed( charactersPerSecond ); -- pass nil for default
 	scene:showDialog( "Hello, welcome to this date", { wobbly = true } );
 	scene:showChoice( { "a", outcomeA, }, { "b", outcomeB, } );
-	
+
+	scene:playSound( gAssets.SOUND.mySound );
+	scene:playMusic( gAssets.MUSIC.mySound );	
 end
